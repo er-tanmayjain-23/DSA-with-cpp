8cd711d450cdad354496a83962b907bffd69f148
Tanmay Jain <er.tanmayjain.5@gmail.com>
2026-08-19 07:05:39 +0530

    Regular Expression Matching.cpp

---
+++ Regular Expression Matching.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty())
+            return s.empty();
+        bool first = !s.empty() && (s[0] == p[0] || p[0] == '.');
+        if (p.length() >= 2 && p[1] == '*') {
+            return isMatch(s, p.substr(2)) ||
+                   (first && isMatch(s.substr(1), p));
+        }
+        return first && isMatch(s.substr(1), p.substr(1));
+    }
+};
